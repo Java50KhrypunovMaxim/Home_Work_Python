fe191b07c11141d80e4c70b13d3b0ba287eb1512
Maxim Khrypunov <khrypunovmax@gmail>
2024-09-11 11:08:05 +0300

    HW-9

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-a = (1, 2, [3, 6], 4, 5)
-a[2][0] = 0
-print(a)
+def sum_of_digits(number):
+    str_number = str(number)
+    if str_number[0] == '-':
+        str_number = str_number[1:]
+    res = sum(int(digit) for digit in str_number)
+    return res
+
+def find_gcd(a, b):
+    res_array = []
+    for i in range(1,100):
+        if a%i == 0 and b%i == 0:
+            res_array.append(i)
+    return max(res_array)
+
+print(find_gcd(15,45))
